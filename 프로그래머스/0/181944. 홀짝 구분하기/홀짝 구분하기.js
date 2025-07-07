0e9f69099cec5ffdd674bb049939c22493aa34b7
gtnwl <59915103+gtnwl@users.noreply.github.com>
2025-07-07 19:52:52 -0400

    [level 0] Title: 홀짝 구분하기, Time: 44.46 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -9,11 +9,8 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
     let [num] = input.map(Number)
-    if(num % 2 === 0){
-      console.log(num,'is even')
-    }else{
-        console.log(num,'is odd')
-    }
+    console.log(num % 2 === 0 ? num + " is even":num + " is odd")
+ 
 }).on('close', function () {
     n = Number(input[0]);
 });
